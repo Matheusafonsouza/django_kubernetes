7d871b9434acf98711fcc860beed2c9d6650d208
matheusafonsouza <matheusafonsouza@gmail.com>
2022-04-15 18:52:40 -0300

    docker compose with redis and postgres

--- migrate.sh
+++ migrate.sh
@@ -2,4 +2,4 @@
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"default@email.com"}
 cd /app/
 python manage.py migrate --noinput
-python manage.py createsuperuser --emaiL $SUPERUSER_EMAIL --noinput || true
+python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
